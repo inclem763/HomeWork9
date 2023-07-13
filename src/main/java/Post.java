b42385307f69396044a050bbee3c9f9b5d68d146
Dmitriy Kozak <dmitrij.kozak.1997@bk.ru>
2023-07-13 09:20:39 +0400

    Post added

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -4,6 +4,6 @@ public class Post {
     public String patronymic;
     public String phone;
     public String surname;
-    public Object subscription;
+    public Boolean subscription;
     public FormDate birthday;
 }
